a32357c1e182089ec0c12f63cd5b8de01cb534c6
shravya k suresh <ksshrav2005@gmail.com>
2023-11-24 01:06:53 +0530

    adding

---
+++ todo_list/todo_app/views.py
@@ -0,0 +1,127 @@
+# todo_list/todo_app/views.py
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    FormView,
+    
+)
+from .models import ToDoItem, ToDoList
+
+class CustomLoginView(LoginView):
+    template_name= 'todo_app/login.html'
+    fields = '_ all_'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+    
+class RegisterPage(FormView):
+    template_name= 'todo_app/register.html'
+    form_class= UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('index')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+class ListListView(LoginRequiredMixin,ListView):
+    model = ToDoList
+    template_name = "todo_app/index.html"
+
+class ItemListView(LoginRequiredMixin,ListView):
+    model = ToDoItem
+    template_name = "todo_app/todo_list.html"
+    
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["todo_list"] = ToDoList.objects.get(id=self.kwargs["list_id"])
+        return context
+
+class ListCreate(LoginRequiredMixin,CreateView):
+    model = ToDoList
+    fields = ["title"]
+
+    def get_context_data(self):
+        context = super(ListCreate, self).get_context_data()
+        context["title"] = "Add a new list"
+        return context
+
+class ItemCreate(LoginRequiredMixin,CreateView):
+    model = ToDoItem
+    fields = [
+        "todo_list",
+        "title",
+        "description",
+        "due_date",
+    ]
+
+    def get_initial(self):
+        initial_data = super(ItemCreate, self).get_initial()
+        todo_list = ToDoList.objects.get(id=self.kwargs["list_id"])
+        initial_data["todo_list"] = todo_list
+        return initial_data
+
+    def get_context_data(self):
+        context = super(ItemCreate, self).get_context_data()
+        todo_list = ToDoList.objects.get(id=self.kwargs["list_id"])
+        context["todo_list"] = todo_list
+        context["title"] = "Create a new item"
+        return context
+
+    def get_success_url(self):
+        return reverse("list", args=[self.object.todo_list_id])
+
+class ItemUpdate(LoginRequiredMixin,UpdateView):
+    model = ToDoItem
+    fields = [
+        "todo_list",
+        "title",
+        "description",
+        "due_date",
+    ]
+
+    def get_context_data(self):
+        context = super(ItemUpdate, self).get_context_data()
+        context["todo_list"] = self.object.todo_list
+        context["title"] = "Edit item"
+        return context
+
+    def get_success_url(self):
+        return reverse("list", args=[self.object.todo_list_id])
+    
+class ListDelete(LoginRequiredMixin,DeleteView):
+    model = ToDoList
+    # You have to use reverse_lazy() instead of reverse(),
+    # as the urls are not loaded when the file is imported.
+    success_url = reverse_lazy("index")
+
+class ItemDelete(LoginRequiredMixin,DeleteView):
+    model = ToDoItem
+
+    def get_success_url(self):
+        return reverse_lazy("list", args=[self.kwargs["list_id"]])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todo_list"] = self.object.todo_list
+        return context
