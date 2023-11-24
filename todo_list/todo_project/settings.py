72a7b28afd1412c107317add1469865f88abf0c1
shravya k suresh <133945587+shraavv@users.noreply.github.com>
2023-11-24 22:38:23 +0530

    Update settings.py (debug = False)

--- todo_list/todo_project/settings.py
+++ todo_list/todo_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c*8lhj(xxnkn!jk77d)---0g($4-5w^v2yk+lppod7$t%_ceqe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []  
 
